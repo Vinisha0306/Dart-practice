f6c28824fc8334b733456905cf9ac4cbbe6b264d
Vinisha0306 <lathiyavinisha03@gmail.com>
2024-01-04 08:59:46 +0530

    04/01/2024

---
+++ user_input_in_Map.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() 
+{
+  stdout.write("Enter Map Size:");
+  int n = int.parse(stdin.readLineSync()??"0");
+  
+  Map<String, dynamic> myMap = {};
+ 
+  for(int i=1;i<=n;i++)
+  {
+    stdout.write("Enter $i key: ");
+    String key =  stdin.readLineSync()??"0";
+    
+    stdout.write("Enter $i value: ");
+    dynamic value = stdin.readLineSync()??"0";
+    
+    myMap[key] = value;
+  };
+  print("");
+  myMap.forEach((key,value) 
+  {
+     print("$key : $value");
+  });
+}
