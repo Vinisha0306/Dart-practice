bdbfc58e7c281e78a081c090090a7d1a3b76827f
Vinisha0306 <lathiyavinisha03@gmail.com>
2024-01-24 11:01:16 +0530

    24/01/2024

--- class2.dart
+++ class2.dart
@@ -1063,16 +1063,3 @@ class Supermarket {
     }
   }
 }
-
-void main() {
-  Supermarket s = Supermarket();
-  print("\tWelcome Supermarket\n");
-  stdout.write("Enter Customer Id:");
-  s.id = int.parse(stdin.readLineSync() ?? "0");
-  stdout.write("Enter Customer Name:");
-  s.name = stdin.readLineSync() ?? "no";
-  stdout.write("Enter Customer Contact No. :");
-  s.contact = int.parse(stdin.readLineSync() ?? "0");
-
-  s.Switch();
-}
